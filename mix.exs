3719ef5f15ac9410df0785bffe6218695c041c90
Josh Chernoff <hello@fullstack.ing>
2025-10-08 10:03:57 -0700

    bump: 0.1.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AetherAtprotoCore.MixProject do
     [
       app: :aether_atproto,
       start_permanent: Mix.env() == :prod,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.18",
       deps: deps(),
       package: [
